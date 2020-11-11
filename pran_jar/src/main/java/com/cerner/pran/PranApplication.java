70e722ce8c62b487e1b5d3d519763d36fe97d684
Abraham <NA075085@cerner.net>
2020-11-11 21:05:21 +0530

    Baseline commit for pran jar

---
+++ pran_jar/src/main/java/com/cerner/pran/PranApplication.java
@@ -0,0 +1,13 @@
+package com.cerner.pran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PranApplication.class, args);
+	}
+
+}
